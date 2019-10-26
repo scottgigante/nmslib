4f997e850d53856db1fa0c69ddab5e26c94e9491
Scott Gigante <scottgigante@gmail.com>
2019-10-26 08:15:18 -0400

    fix pypi and appveyor deployment

---
+++ travis/build-wheels.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+yum install -y atlas-devel
+
+cwd=$(pwd)
+cd /io/python_bindings
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    if [ $("${PYBIN}/python" --version | sed 's/\.//g' | grep -c "Python ${PYTHON}") -eq 0 ]; then
+        continue
+    fi
+    "${PYBIN}/pip" install -r dev-requirements.txt
+    "${PYBIN}/python" setup.py build_ext
+    "${PYBIN}/pip" wheel . -w ${cwd}/wheelhouse/
+done
+cd $cwd
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/python_bindings/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    if [ $("${PYBIN}/python" --version | sed 's/\.//g' | grep -c "Python ${PYTHON}") -eq 0 ]; then
+        continue
+    fi
+    "${PYBIN}/pip" install nmspy --no-index -f /io/python_bindings/wheelhouse
+    cd /io/python_bindings/tests/
+    "${PYBIN}/python" -m pytest
+done
+
